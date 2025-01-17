5dfeef2246eb57c9d24bce76814e24bb7bde6c2e
FalconSlayer51 <dhanrajramesh22@gmail.com>
2025-01-17 13:40:46 +0530

    leetcode day 95 approach 2

--- src/leetcode_daily/NeighboringBitwiseOr.java
+++ src/leetcode_daily/NeighboringBitwiseOr.java
@@ -2,6 +2,27 @@ package leetcode_daily;
 
 
 public class NeighboringBitwiseOr {
+
+    public boolean doesValidArrayExist1(int[] derived) {
+        int n = derived.length;
+        int[] original = new int[n];
+
+        original[0] = 0;
+        for (int i = 0; i < n-1; i++) {
+            original[i+1] = derived[i] ^ original[i];
+        }
+
+        if((original[n-1] ^ original[0]) == 0) return true;
+
+        original[0] = 1;
+        for (int i = 0; i < n-1; i++) {
+            original[i+1] = derived[i] ^ original[i];
+        }
+
+        if((original[n-1] ^ original[0]) == derived[n-1]) return true;
+
+        return false;
+    }
     public boolean doesValidArrayExist(int[] derived) {
         int xorSum = 0;
         for (int num : derived) {
@@ -28,7 +49,7 @@ public class NeighboringBitwiseOr {
         NeighboringBitwiseOr solution = new NeighboringBitwiseOr();
 
         for (int i = 0; i < testCases.length; i++) {
-            boolean result = solution.doesValidArrayExist(testCases[i]);
+            boolean result = solution.doesValidArrayExist1(testCases[i]);
             System.out.println("Test case " + (i + 1) + ": " + (result == expectedResults[i] ? "Passed" : "Failed"));
         }
     }
