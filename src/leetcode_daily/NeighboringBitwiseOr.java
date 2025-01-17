f88606b85f951f2851f1bf329aba84a9e38d8e49
FalconSlayer51 <dhanrajramesh22@gmail.com>
2025-01-17 13:43:14 +0530

    leetcode day 95 approach 2 some changes

--- src/leetcode_daily/NeighboringBitwiseOr.java
+++ src/leetcode_daily/NeighboringBitwiseOr.java
@@ -12,7 +12,7 @@ public class NeighboringBitwiseOr {
             original[i+1] = derived[i] ^ original[i];
         }
 
-        if((original[n-1] ^ original[0]) == 0) return true;
+        if((original[n-1] ^ original[0]) == derived[n-1]) return true;
 
         original[0] = 1;
         for (int i = 0; i < n-1; i++) {
